e7d2a89fa85d5b4eb8b24ae90c0413a8e422dd96
guzzyman <gbenga.isess@gmail.com>
2023-06-09 06:20:54 +0100

    Updated the event details, implemented a modal

---
+++ components/EventDetails.jsx
@@ -0,0 +1,37 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
+import Image from 'next/image';
+import React from 'react'
+
+const EventDetails = (props) => {
+  const { title, open, onClose, ticketDetails } = props;
+  return (
+    <Dialog open={open} fullWidth>
+      <DialogTitle>Event Category: {ticketDetails?.title}</DialogTitle>
+      <DialogContent className="">
+        <div className='flex flex-row gap-6'>
+          <Image
+            className="inset-0 rounded-lg"
+            src={ticketDetails?.dataImage}
+            alt="background image"
+            width={240}
+            height={240}
+          />
+          <div className="grid gap-4 md:grid-cols-2lg:grid-cols-2 py-4 justify-between">
+            <span>Ticket Details:</span> {ticketDetails?.description}
+            <span>Ticket Price:</span> {ticketDetails?.price}
+          </div>
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="outlined" color="error" onClick={onClose}>
+          Cancel
+        </Button>
+        <Button className="flex-row justify-center items-center w-[200px] p-[18px] text-white text-sm rounded-lg bg-gradient-to-r from-[#2472FF] to-[#7E51DB] inline-flex">
+          <span className='text-white'> Buy Ticket</span>
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default EventDetails
--- components/MoreEventSection.jsx
+++ components/MoreEventSection.jsx
@@ -1,93 +1,109 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { EventBackgroundEnum } from "./Constants";
 import Image from "next/image";
+import EventDetails from "./EventDetails";
 
 const MoreEventSection = (props) => {
   const { title } = props;
-  return (
-    <section
-      className={`w-full p-5 pb-20 px-20 grid grid-cols-3 gap-6 min-h-[400px] ${EventBackgroundEnum.WHITE}`}
-    >
-      <div className="text-4xl text-black col-span-3">{title}</div>
-      <div className="flex flex-row col-span-3 gap-2">
-        {moreEventList?.map((item, index) => (
-          <button
-            key={index}
-            className="text-xl text-white bg-slate-400 bg-opacity-100 px-6 py-2 w-max rounded-3xl"
-          >
-            {item?.value}
-          </button>
-        ))}
-      </div>
+  const [openModal, setOpenModal] = useState(false);
+  const [ticketDetails, setTicketDetails] = useState(null);
+  const trendingEvents = [
+    {
+      dataImage: "/assets/images/sampleImage.jpg",
+      title: "Sport",
+      description: "American Football",
+      eventID: 1,
+      price: 0.1,
+    },
+    {
+      dataImage: "/assets/images/sampleImage2.jpg",
+      title: "Concert",
+      description: "Wizkid Made in Lagos Concert",
+      eventID: 2,
+      price: 0.1,
+    },
+    {
+      dataImage: "/assets/images/sampleImage3.jpg",
+      title: "Travels",
+      description: "A trip to Greenland",
+      eventID: 3,
+      price: 0.1,
+    },
+    {
+      dataImage: "/assets/images/sampleImage.jpg",
+      title: "Sport",
+      description: "American Football",
+      eventID: 4,
+      price: 0.1,
+    },
+    {
+      dataImage: "/assets/images/sampleImage2.jpg",
+      title: "Concert",
+      description: "Wizkid Made in Lagos Concert",
+      eventID: 5,
+      price: 0.1,
+    },
+    {
+      dataImage: "/assets/images/sampleImage3.jpg",
+      title: "Travels",
+      description: "A trip to Greenland",
+      eventID: 6,
+      price: 0.1,
+    },
+  ];
 
-      {trendingEvents?.map((item, index) => (
-        <div className="relative" key={index}>
-          <Image
-            className="absolute inset-0 rounded-2xl"
-            src={item?.dataImage}
-            alt="background image"
-            fill
-          />
-          <div className="relative z-10 flex flex-col w-72 h-[266px] p-6">
-            <button className="text-xl font-bold text-gray-200 bg-slate-400 bg-opacity-75 px-6 py-2 w-max">
-              {item?.title}
+  return (
+    <>
+      {openModal && (
+        <EventDetails
+          title="Event Details"
+          open={openModal}
+          ticketDetails={ticketDetails}
+          onClose={() => setOpenModal(false)}
+        />
+      )}
+      <section
+        className={`w-full p-5 pb-20 px-20 grid grid-cols-3 gap-6 min-h-[400px] ${EventBackgroundEnum.WHITE}`}
+      >
+        <div className="text-4xl text-black col-span-3">{title}</div>
+        <div className="flex flex-row col-span-3 gap-2">
+          {moreEventList?.map((item, index) => (
+            <button
+              key={index}
+              className="text-xl text-white bg-slate-400 bg-opacity-100 px-6 py-2 w-max rounded-3xl"
+            >
+              {item?.value}
             </button>
-            <div className="mt-4 text-white bottom-0 absolute text-2xl pb-4">
-              {item?.description}
+          ))}
+        </div>
+
+        {trendingEvents?.map((item, index) => (
+          <div className="relative" key={index} onClick={() => {
+            setOpenModal(true);
+            setTicketDetails(item);
+          }}>
+            <Image
+              className="absolute inset-0 rounded-2xl"
+              src={item?.dataImage}
+              alt="background image"
+              fill
+            />
+            <div className="relative z-10 flex flex-col w-72 h-[266px] p-6">
+              <button className="text-xl font-bold text-gray-200 bg-slate-400 bg-opacity-75 px-6 py-2 w-max">
+                {item?.title}
+              </button>
+              <div className="mt-4 text-white bottom-0 absolute text-2xl pb-4">
+                {item?.description}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </section>
+        ))}
+      </section></>
+
   );
 };
 
 export default MoreEventSection;
-
-const trendingEvents = [
-  {
-    dataImage: "/assets/images/sampleImage.jpg",
-    title: "Sport",
-    description: "American Football",
-    eventID: 1,
-    price: 0.1,
-  },
-  {
-    dataImage: "/assets/images/sampleImage2.jpg",
-    title: "Concert",
-    description: "Wizkid Made in Lagos Concert",
-    eventID: 2,
-    price: 0.1,
-  },
-  {
-    dataImage: "/assets/images/sampleImage3.jpg",
-    title: "Travels",
-    description: "A trip to Greenland",
-    eventID: 3,
-    price: 0.1,
-  },
-  {
-    dataImage: "/assets/images/sampleImage.jpg",
-    title: "Sport",
-    description: "American Football", 
-    eventID: 4,
-    price: 0.1,
-  },
-  {
-    dataImage: "/assets/images/sampleImage2.jpg",
-    title: "Concert",
-    description: "Wizkid Made in Lagos Concert", 
-    eventID: 5,
-    price: 0.1,
-  },
-  {
-    dataImage: "/assets/images/sampleImage3.jpg",
-    title: "Travels",
-    description: "A trip to Greenland", 
-    eventID: 6,
-    price: 0.1,
-  },
-];
 
 const moreEventList = [
   { value: "All" },
